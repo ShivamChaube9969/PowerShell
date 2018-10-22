7f96812a017c296ee55434df3922197227f1a525
Shivam Chaube <shivam.chaube@metlife.com>
2018-10-22 06:28:01 -0400

    Working on time stamp

---
+++ FileDate.ps1
@@ -0,0 +1,20 @@
+﻿Write-Host "Output starts Here"
+$FilePath = "D:\Users\schaube\Documents\WindowsPowerShell\Test.txt"
+$fileExists = Test-Path "$FilePath"
+
+if($fileExists) {
+    $FileContent = Get-Content "$FilePath"
+    Write-Host "$FileContent"
+    $DateTime = [datetime](Get-ItemProperty -Path $FilePath -Name LastWriteTime).lastwritetime
+    $Var = "$DateTime"
+    $TrimLength = 0
+    $FinalLength = 10
+    $Date = $Var.SubString($TrimLength,$FinalLength)
+    Write-Host "Time Stamp for the given file is : "$Date
+    }
+else
+    {
+    Write-Host "The Test file does not exist"  
+    exit(0) 
+    }
+
---
+++ FilePathTest.ps1
@@ -0,0 +1,19 @@
+﻿Write-Host "Output starts Here"
+$FilePath = "D:\Users\schaube\Documents\WindowsPowerShell\Test.txt"
+$fileExists = Test-Path "$FilePath"
+
+if($fileExists) {
+    $FileContent = Get-Content "$FilePath"
+    Write-Host "$FileContent"
+    $DateTime = [datetime](Get-ItemProperty -Path $FilePath -Name LastWriteTime).lastwritetime
+    $Start = 0
+    $End = 10
+    $Date = $DateTime.SubString($TrimLength,$FinalLength)
+    Write-Host "Time Stamp for the given file is : "$DateTime
+    }
+else
+    {
+    Write-Host "The Test file does not exist"  
+    exit(0) 
+    }
+
